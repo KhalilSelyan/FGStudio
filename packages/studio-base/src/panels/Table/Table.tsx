ad565b6efeee4731839783c53b61fe4214b8bf48
Andrew Carr <andrew@foxglove.dev>
2022-06-17 15:32:46 +1000

    Use outlined variant of select on panel/Table

--- packages/studio-base/src/panels/Table/Table.tsx
+++ packages/studio-base/src/panels/Table/Table.tsx
@@ -279,7 +279,6 @@ export default function Table({
             </IconButton>
             <Select
               value={pageSize}
-              variant="filled"
               size="small"
               onChange={(e) => setPageSize(Number(e.target.value))}
               MenuProps={{ MenuListProps: { dense: true } }}
