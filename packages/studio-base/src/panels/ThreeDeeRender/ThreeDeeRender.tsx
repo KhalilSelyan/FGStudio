8eeac28e58b9d1eaf9d315e18c2084a5f2d5e426
John Hurliman <jhurliman@jhurliman.org>
2022-06-20 14:00:32 -0700

    cleanup

--- packages/studio-base/src/panels/ThreeDeeRender/ThreeDeeRender.tsx
+++ packages/studio-base/src/panels/ThreeDeeRender/ThreeDeeRender.tsx
@@ -260,13 +260,7 @@ export function ThreeDeeRender({ context }: { context: PanelExtensionContext }):
       return;
     }
     log.debug(`Subscribing to [${topicsToSubscribe.join(", ")}]`);
-
-    context.subscribe(
-      topicsToSubscribe.map((topic) => ({
-        topic,
-        preload: false,
-      })),
-    );
+    context.subscribe(topicsToSubscribe.map((topic) => ({ topic, preload: false })));
   }, [context, topicsToSubscribe]);
 
   // Keep the renderer currentTime up to date
